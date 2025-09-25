8acf98473e30e77f875043448509f869ee620985
jiiquu <74710058+jiiquu@users.noreply.github.com>
2025-09-25 10:13:32 +0300

    Trying to exclude desktop styling from mobile layout, changing font loading method

--- JS/scripts.js
+++ JS/scripts.js
@@ -20,36 +20,36 @@ form.addEventListener('submit', async (e) => {
         showContainer.innerHTML = 'Error fetching data.';
         console.error(error);
     }
-    
-    function populateShows(shows) {
-        if (shows.length === 0) {
-            showContainer.innerHTML = 'No shows found.';
-            return;
-        };
-        shows.forEach(item => {
-            const show = item.show;
-            const showDiv = document.createElement('div');
-            showDiv.className = 'show-data';
-            const img = document.createElement('img');
-            img.src = show?.image?.medium || '/img/no-image.png';
-            img.alt = show.name;
-            const info = document.createElement('div');
-            info.className = 'show-info';
-            const title = document.createElement('h1');
-            title.textContent = show?.name ?? 'No title available';
-            const summary = document.createElement('div');
-            summary.innerHTML = show?.summary || 'No summary available.';
-            info.appendChild(title);
-            info.appendChild(summary);
-            showDiv.appendChild(img);
-            showDiv.appendChild(info);
-            showContainer.appendChild(showDiv);
-        }
-        )
-        
+})    
+function populateShows(shows) {
+    if (shows.length === 0) {
+        showContainer.innerHTML = 'No shows found.';
+        return;
+    };
+    shows.forEach(item => {
+        const show = item.show;
+        const showDiv = document.createElement('div');
+        showDiv.className = 'show-data';
+        const img = document.createElement('img');
+        img.src = show?.image?.medium || '/img/no-image.png';
+        img.alt = show.name;
+        const info = document.createElement('div');
+        info.className = 'show-info';
+        const title = document.createElement('h1');
+        title.textContent = show?.name ?? 'No title available';
+        const summary = document.createElement('div');
+        summary.innerHTML = show?.summary || 'No summary available.';
+        info.appendChild(title);
+        info.appendChild(summary);
+        showDiv.appendChild(img);
+        showDiv.appendChild(info);
+        showContainer.appendChild(showDiv);
     }
+    )
+    
+}
 
-})
+
 
 // default search on page load
 /* document.addEventListener('DOMContentLoaded', () => {
